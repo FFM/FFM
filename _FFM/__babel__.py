0ca53d46e8b99d26aa38c757be782e097c3dd0c8
Christian Tanzer <tanzer@swing.co.at>
2012-03-28 09:08:18 +0200

    Added translations for FFM (and the boiler plate __babel__.py I forgot yesterday).

---
+++ _FFM/__babel__.py
@@ -0,0 +1,50 @@
+# -*- coding: iso-8859-15 -*-
+# Copyright (C) 2012 Mag. Christian Tanzer All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# #*** <License> ************************************************************#
+# This module is part of the package FFM.
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this module. If not, see <http://www.gnu.org/licenses/>.
+# #*** </License> ***********************************************************#
+#
+#++
+# Name
+#    FFM.__babel__
+#
+# Purpose
+#    This file is the entry point for the Babel translation extraction
+#    process.
+#
+# Revision Dates
+#    28-Mar-2012 (CT) Creation
+#    ««revision-date»»···
+#--
+
+from   __future__  import absolute_import, division, print_function, unicode_literals
+
+from   _MOM.import_MOM import *
+import _MOM.Babel
+import _FFM.import_FFM
+
+def main (encoding, config, method) :
+    from   _MOM._EMS.Hash         import Manager as EMS
+    from   _MOM._DBW._HPS.Manager import Manager as DBW
+    from   _FFM import FFM
+    return MOM.Babel.Add_Translations \
+        ( encoding, config, method
+        , MOM.App_Type ("FFM", FFM).Derived (EMS, DBW)
+        )
+# end def main
+
+### __END__ FFM.__babel__
