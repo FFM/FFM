c84c4d351a8938506ba5c90bceb8a7cd674aeeaf
Ralf Schlatterbeck <rsc@runtux.com>
2012-09-12 15:36:09 +0200

    Use `derive_pns_bases`.

--- _FFM/Entity.py
+++ _FFM/Entity.py
@@ -29,74 +29,20 @@
 #     6-Mar-2012 (CT) Creation
 #    10-May-2012 (CT) Add `An_Entity`, `Id_Entity`, `Object`, `Link1`, `Link2`
 #    17-May-2012 (CT) Fix typo (`Common`, not `Command`)
+#    12-Sep-2012 (RS) Use `derive_pns_bases`
 #    ««revision-date»»···
 #--
 
 from   __future__  import absolute_import, division, print_function, unicode_literals
 
+from   _GTW                   import GTW
 from   _MOM.import_MOM        import *
+
+import _MOM.derive_pns_bases
+import _FFM
 from   _FFM                   import FFM
 
-_Ancestor_Essence = MOM.Entity
-
-class _FFM_Entity_ (_Ancestor_Essence) :
-    """Common base class for essential classes of FFM"""
-
-    _real_name = "Entity"
-    is_partial = True
-    PNS        = FFM
-
-Entity = _FFM_Entity_ # end class
-
-_Ancestor_Essence = MOM.An_Entity
-
-class _FFM_An_Entity_ (Entity, _Ancestor_Essence) :
-    """Common base class for essential classes of FFM"""
-
-    _real_name = "An_Entity"
-    is_partial = True
-
-An_Entity = _FFM_An_Entity_ # end class
-
-_Ancestor_Essence = MOM.Id_Entity
-
-class _FFM_Id_Entity_ (Entity, _Ancestor_Essence) :
-    """Common base class for essential classes of FFM"""
-
-    _real_name = "Id_Entity"
-    is_partial = True
-
-Id_Entity = _FFM_Id_Entity_ # end class
-
-_Ancestor_Essence = MOM.Object
-
-class _FFM_Object_ (Entity, _Ancestor_Essence) :
-    """Common base class for essential object classes of FFM."""
-
-    _real_name = "Object"
-    is_partial = True
-
-Object = _FFM_Object_ # end class
-
-_Ancestor_Essence = MOM.Link1
-
-class _FFM_Link1_ (Entity, _Ancestor_Essence) :
-    """Common base class for essential unary link classes of FFM."""
-
-    _real_name = "Link1"
-    is_partial = True
-
-Link1 = _FFM_Link1_ # end class
-
-_Ancestor_Essence = MOM.Link2
-
-class _FFM_Link2_ (Entity, _Ancestor_Essence) :
-    """Common base class for essential binary link classes of FFM."""
-
-    _real_name = "Link2"
-    is_partial = True
-
-Link2 = _FFM_Link2_ # end class
+MOM.derive_pns_bases (FFM, MOM)
 
 if __name__ != "__main__" :
     FFM._Export ("*")
