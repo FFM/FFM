0012362ab6aaf69caff4d056d8b367c29a116b92
Ralf Schlatterbeck <rsc@runtux.com>
2013-01-20 18:17:11 +0100

    Better error checking, spider refactored

--- spider/spiderpool.py
+++ spider/spiderpool.py
@@ -2,45 +2,72 @@ import os
 
 from multiprocessing  import Pool, Manager
 from rsclib.autosuper import autosuper
+from rsclib.execute   import Log
 from olsr.parser      import OLSR_Parser
 from spider.parser    import Guess
+from itertools        import islice
 
 def get_node_info (result_dict, ip) :
+    w = Worker (result_dict, ip)
     try :
-        if ip in result_dict :
-            return
-        try :
-            url  = ''
-            site = Guess.site % locals ()
-            #site = 'file:///' + os.path.abspath ('spider/%s' % ip)
-            #url  = 'index.html'
-            g    = Guess (site = site, url = 'index.html')
-        except ValueError, err :
-            result_dict [ip] = ('ValueError', err)
-            return
-        result = []
-        for iface_ip in g.ips.iterkeys () :
-            iface = iface_ip.iface
-            r = [iface.name]
-            if iface.is_wlan :
-                r.extend \
-                    (( True
-                    ,  iface.wlan_info.ssid
-                    ,  iface.wlan_info.mode
-                    ,  iface.wlan_info.channel
-                    ,  iface.wlan_info.bssid
-                    ))
-            else :
-                r.append (False)
-            result.append (r)
-        result_dict [ip] = result
+        return w.get_node_info ()
     except Exception, err :
-        result_dict [ip] = ("ERROR", err)
+        w.log.log_exception ()
 # end def get_node_info
 
-class Spider (autosuper) :
+class Worker (Log) :
 
-    def __init__ (self, olsr_file) :
+    def __init__ (self, result_dict, ip, **kw) :
+        self.__super.__init__ (** kw)
+        self.ip          = ip
+        self.result_dict = result_dict
+    # end def __init__
+
+    def get_node_info (self) :
+        try :
+            if self.ip in self.result_dict :
+                return
+            try :
+                url  = ''
+                site = Guess.site % self.__dict__
+                #site = 'file:///' + os.path.abspath ('spider/%s' % self.ip)
+                #url  = 'index.html'
+                self.log.debug ("%s: before guess" % self.ip)
+                g    = Guess (site = site, url = 'index.html')
+                self.log.debug ("%s: after  guess" % self.ip)
+            except ValueError, err :
+                self.result_dict [self.ip] = ('ValueError', err)
+                return
+            except :
+                self.log_exception ()
+                return
+            result = []
+            for iface_ip in g.ips.iterkeys () :
+                iface = iface_ip.iface
+                r = [iface.name]
+                if iface.is_wlan :
+                    r.extend \
+                        (( True
+                        ,  iface.wlan_info.ssid
+                        ,  iface.wlan_info.mode
+                        ,  iface.wlan_info.channel
+                        ,  iface.wlan_info.bssid
+                        ))
+                else :
+                    r.append (False)
+                result.append (r)
+            self.result_dict [self.ip] = result
+        except Exception, err :
+            self.log.error (err)
+            self.result_dict [self.ip] = ("ERROR", err)
+    # end def get_node_info
+
+# end class Worker
+
+class Spider (Log) :
+
+    def __init__ (self, olsr_file, **kw) :
+        self.__super.__init__ (**kw)
         olsr_parser = OLSR_Parser ()
         olsr_parser.parse (open (olsr_file))
         self.olsr_nodes = {}
@@ -48,7 +75,11 @@ class Spider (autosuper) :
             self.olsr_nodes [t] = True
         for t in olsr_parser.topo.reverse.iterkeys () :
             self.olsr_nodes [t] = True
-        self.pool  = Pool (processes = 50)
+        # limit to N elements
+        N = 40
+        self.olsr_nodes = dict \
+            ((k, v) for k, v in islice (self.olsr_nodes.iteritems (), N))
+        self.pool  = Pool (processes = 20)
         self.mgr   = Manager ()
         self.result_dict = self.mgr.dict ()
         olsr_nodes = None
@@ -75,11 +106,20 @@ if __name__ == '__main__' :
     if len (sys.argv) > 1 :
         name = sys.argv [1]
     olsr_file = 'olsr/txtinfo.txt'
+    # single ip test
+    #d = dict ()
+    #w = Worker (d, '193.238.156.32')
+    #w.get_node_info ()
+    #print d
+    #sys.exit (23)
     sp = Spider (olsr_file)
-    sp.process ()
-    f = open (name, "wb")
-    pickle.dump (sp.result_dict, f)
-    f.close ()
-    for k, v in sorted \
-        (sp.result_dict.iteritems (), key = lambda z : (z [1][0], z [0])) :
-        print k, v
+    try :
+        sp.process ()
+        f = open (name, "wb")
+        pickle.dump (sp.result_dict, f)
+        f.close ()
+        for k, v in sorted \
+            (sp.result_dict.iteritems (), key = lambda z : (z [1][0], z [0])) :
+            print k, v
+    except Exception, err :
+        sp.log_exception ()
