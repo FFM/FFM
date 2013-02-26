2e8eacde4055be62249ab66c506ed48461548aba
Christian Tanzer <tanzer@swing.co.at>
2013-02-26 09:26:42 +0100

    Add `Virtual_Wireless_Interface`

--- _FFM/Wireless_Interface.py
+++ _FFM/Wireless_Interface.py
@@ -1,5 +1,5 @@
 # -*- coding: iso-8859-15 -*-
-# Copyright (C) 2012 Mag. Christian Tanzer All rights reserved
+# Copyright (C) 2012-2013 Mag. Christian Tanzer All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # #*** <License> ************************************************************#
 # This module is part of the package FFM.
@@ -36,6 +36,8 @@
 #    17-Dec-2012 (RS) Add `auto_cache` for `left`
 #    17-Dec-2012 (CT) Add attribute `mode`
 #    17-Dec-2012 (CT) Set `standard.ui_allow_new` to `False`
+#    26-Feb-2013 (CT) Add `Virtual_Wireless_Interface`,
+#                     factor `_Wireless_Interface_`
 #    ««revision-date»»···
 #--
 
@@ -52,8 +54,10 @@ import _FFM.Wireless_Standard
 
 _Ancestor_Essence = FFM.Net_Interface
 
-class Wireless_Interface (_Ancestor_Essence) :
-    """Wireless interface of a FFM device"""
+class _Wireless_Interface_ (_Ancestor_Essence) :
+    """Base class for wireless interfaces"""
+
+    is_partial  = True
 
     class _Attributes (_Ancestor_Essence._Attributes) :
 
@@ -75,23 +79,6 @@ class Wireless_Interface (_Ancestor_Essence) :
 
         # end class mode
 
-        class standard (A_Id_Entity) :
-            """Wireless standard used by the wireless interface."""
-
-            kind               = Attr.Necessary
-            P_Type             = FFM.Wireless_Standard
-            ui_allow_new       = False
-
-        # end class standard
-
-        class txpower (A_TX_Power) :
-            """Transmit power with unit (units of dBW or W)."""
-
-            kind               = Attr.Optional
-            ui_name            = "TX power"
-
-        # end class power
-
         class essid (A_String) :
             """Network name."""
 
@@ -111,17 +98,87 @@ class Wireless_Interface (_Ancestor_Essence) :
 
         # end class bssid
 
-        ### MAC parameters
-        class is_hidden (A_Boolean) :
-            """???"""
+        class standard (A_Id_Entity) :
+            """Wireless standard used by the wireless interface."""
+
+            kind               = Attr.Necessary
+            P_Type             = FFM.Wireless_Standard
+            ui_allow_new       = False
+
+        # end class standard
+
+        class txpower (A_TX_Power) :
+            """Transmit power with unit (units of dBW or W)."""
 
             kind               = Attr.Optional
+            ui_name            = "TX power"
 
-        # end class is_hidden
+        # end class power
 
     # end class _Attributes
 
+# end class _Wireless_Interface_
+
+_Ancestor_Essence = _Wireless_Interface_
+
+class Wireless_Interface (_Ancestor_Essence) :
+    """Wireless interface of a FFM device"""
+
 # end class Wireless_Interface
+
+_Ancestor_Essence = _Wireless_Interface_
+
+class Virtual_Wireless_Interface (_Ancestor_Essence) :
+    """Virtual wireless interface of a FFM device"""
+
+    class _Attributes (_Ancestor_Essence._Attributes) :
+
+        _Ancestor = _Ancestor_Essence._Attributes
+
+        class hardware (A_Id_Entity) :
+            """Hardware interface used by virtual interface."""
+
+            kind               = Attr.Primary
+            P_Type             = Wireless_Interface
+
+        # end class hardware
+
+        ### Non-primary attributes
+
+        class standard (_Ancestor.standard) :
+
+            kind               = Attr.Query
+            auto_up_depends    = ("hardware", )
+            query              = Q.hardware.standard
+
+        # end class standard
+
+        class txpower (_Ancestor.txpower) :
+
+            kind               = Attr.Query
+            auto_up_depends    = ("hardware", )
+            query              = Q.hardware.txpower
+
+        # end class power
+
+    # end class _Attributes
+
+    class _Predicates (_Ancestor_Essence._Predicates) :
+
+        _Ancestor = _Ancestor_Essence._Predicates
+
+        class valid_left (Pred.Condition) :
+            """`left` must be equal to `hardware.left`"""
+
+            kind               = Pred.Object
+            assertion          = "left is hardware.left"
+            attributes         = ("left", "hardware.left")
+
+        # end class valid_left
+
+    # end class _Predicates
+
+# end class Virtual_Wireless_Interface
 
 if __name__ != "__main__" :
     FFM._Export ("*")
