e9b6d5389bcbd622d7424155da5809ec17488d8a
Christian Tanzer <tanzer@swing.co.at>
2013-05-03 18:51:56 +0200

    Add test for `META` query argument

--- _FFM/__test__/RST.py
+++ _FFM/__test__/RST.py
@@ -30,6 +30,7 @@
 #     1-Feb-2013 (RS) Fix rounding error with python2.6
 #     5-Mar-2013 (CT) Adapt to changes in `Net_Interface_in_IP4_Network`
 #    28-Mar-2013 (CT) Factor `_test_limit` from `_test_get`
+#     3-May-2013 (CT) Add test for `META` query argument
 #    ««revision-date»»···
 #--
 
@@ -307,6 +308,143 @@ _test_limit = r"""
         }
     , 'status' : 200
     , 'url' : 'http://localhost:9999/v1/FFM-Net_Interface_in_IP4_Network?verbose&closure&order_by=pid&limit=1'
+    }
+
+    >>> r = show (R.get ("/v1/FFM-Net_Interface_in_IP4_Network?verbose&closure&order_by=pid&limit=1&META"), cleaner = date_cleaner)
+    { 'json' :
+        { 'attribute_names' :
+            [ 'left.pid'
+            , 'left.url'
+            , 'right.pid'
+            , 'right.url'
+            , 'mask_len'
+            ]
+        , 'entries' :
+            [ { 'attributes' :
+                  { 'left' :
+                      { 'attributes' :
+                          { 'left' :
+                              { 'attributes' :
+                                  { 'left' :
+                                      { 'attributes' :
+                                          { 'model_no' : ''
+                                          , 'name' : 'generic'
+                                          , 'revision' : ''
+                                          }
+                                      , 'cid' : 42
+                                      , 'creation' :
+                                          { 'date' : <datetime> }
+                                      , 'last_change' :
+                                          { 'date' : <datetime> }
+                                      , 'pid' : 27
+                                      , 'type_name' : 'FFM.Net_Device_Type'
+                                      , 'url' : '/v1/FFM-Net_Device_Type/27'
+                                      }
+                                  , 'name' : 'dev'
+                                  , 'node' :
+                                      { 'attributes' :
+                                          { 'address' :
+                                              { 'attributes' :
+                                                  { 'city' : 'wien'
+                                                  , 'country' : 'austria'
+                                                  , 'street' : 'beispiel 23'
+                                                  , 'zip' : '1010'
+                                                  }
+                                              , 'cid' : 50
+                                              , 'creation' :
+                                                  { 'date' : <datetime> }
+                                              , 'last_change' :
+                                                  { 'date' : <datetime> }
+                                              , 'pid' : 35
+                                              , 'type_name' : 'PAP.Address'
+                                              , 'url' : '/v1/PAP-Address/35'
+                                              }
+                                          , 'manager' :
+                                              { 'attributes' :
+                                                  { 'first_name' : 'ralf'
+                                                  , 'last_name' : 'schlatterbeck'
+                                                  , 'middle_name' : ''
+                                                  , 'title' : ''
+                                                  }
+                                              , 'cid' : 1
+                                              , 'creation' :
+                                                  { 'date' : <datetime> }
+                                              , 'last_change' :
+                                                  { 'date' : <datetime> }
+                                              , 'pid' : 1
+                                              , 'type_name' : 'PAP.Person'
+                                              , 'url' : '/v1/PAP-Person/1'
+                                              }
+                                          , 'name' : 'node2'
+                                          , 'owner' :
+                                              { 'pid' : 1
+                                              , 'url' : '/v1/PAP-Person/1'
+                                              }
+                                          , 'position' :
+                                              { 'lat' : 48.25
+                                              , 'lon' : 15.8744
+                                              }
+                                          }
+                                      , 'cid' : 51
+                                      , 'creation' :
+                                          { 'date' : <datetime> }
+                                      , 'last_change' :
+                                          { 'date' : <datetime> }
+                                      , 'pid' : 3
+                                      , 'type_name' : 'FFM.Node'
+                                      , 'url' : '/v1/FFM-Node/3'
+                                      }
+                                  }
+                              , 'cid' : 43
+                              , 'creation' :
+                                  { 'date' : <datetime> }
+                              , 'last_change' :
+                                  { 'date' : <datetime> }
+                              , 'pid' : 28
+                              , 'type_name' : 'FFM.Net_Device'
+                              , 'url' : '/v1/FFM-Net_Device/28'
+                              }
+                          , 'mac_address' : ''
+                          , 'name' : 'wr'
+                          }
+                      , 'cid' : 44
+                      , 'creation' :
+                          { 'date' : <datetime> }
+                      , 'last_change' :
+                          { 'date' : <datetime> }
+                      , 'pid' : 29
+                      , 'type_name' : 'FFM.Wired_Interface'
+                      , 'url' : '/v1/FFM-Wired_Interface/29'
+                      }
+                  , 'mask_len' : 24
+                  , 'right' :
+                      { 'attributes' :
+                          { 'net_address' :
+                              { 'address' : '192.168.23.1' }
+                          }
+                      , 'cid' : 29
+                      , 'creation' :
+                          { 'date' : <datetime> }
+                      , 'last_change' :
+                          { 'date' : <datetime> }
+                      , 'pid' : 20
+                      , 'type_name' : 'FFM.IP4_Network'
+                      , 'url' : '/v1/FFM-IP4_Network/20'
+                      }
+                  }
+              , 'cid' : 46
+              , 'creation' :
+                  { 'date' : <datetime> }
+              , 'last_change' :
+                  { 'date' : <datetime> }
+              , 'pid' : 31
+              , 'type_name' : 'FFM.Wired_Interface_in_IP4_Network'
+              , 'url' : '/v1/FFM-Net_Interface_in_IP4_Network/31'
+              }
+            ]
+        }
+    , 'status' : 200
+    , 'url' : 'http://localhost:9999/v1/FFM-Net_Interface_in_IP4_Network?verbose&closure&order_by=pid&limit=1&META'
     }
 
 """
