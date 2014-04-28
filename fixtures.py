3e4db000e96267ec49f9cac0587bf8d10987cbb7
Christian Tanzer <tanzer@swing.co.at>
2014-04-28 19:48:58 +0200

    Add account `aaron@lo-res.org`, group `FFM-admin`

--- fixtures.py
+++ fixtures.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2012-2013 Mag. Christian Tanzer All rights reserved
+# Copyright (C) 2012-2014 Mag. Christian Tanzer All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # #*** <License> ************************************************************#
 # This module is part of the program FFM.
@@ -28,6 +28,7 @@
 # Revision Dates
 #    17-Dec-2012 (RS) Creation, move old fixtures.py to _FFM
 #    27-May-2013 (CT) Remove trivial `password` values
+#    28-Apr-2014 (CT) Add account `aaron@lo-res.org` and group `FFM-admin`
 #    ««revision-date»»···
 #--
 
@@ -45,7 +46,12 @@ def create (scope) :
         ( "tanzer@swing.co.at"
         , enabled = True, suspended = False
         )
+    Auth.Account.create_new_account_x \
+        ( "aaron@lo-res.org"
+        , enabled = True, superuser = True, suspended = False
+        )
     Auth.Group ("FFM")
+    Auth.Group ("FFM-admin")
     Auth.Account_in_Group ("tanzer@swing.co.at", "FFM")
 # end def create
 
